88923ffb086376e5a55da8a85c1c01b7492f78b7
David Olejnik <davydon@me.com>
2020-04-23 00:38:46 +0200

    Update SignalR and fixed all results count

--- TaskBoard/ViewModels/BaseResultsModel.cs
+++ TaskBoard/ViewModels/BaseResultsModel.cs
@@ -4,13 +4,19 @@ namespace TaskBoard.ViewModels
 {
     public class BaseResultsModel<T> where T : class
     {
-        public int AllResultsCount { get; }
+        public int AllResultsCount { get; private set; }
         public List<T> Results { get; }
 
         public BaseResultsModel(int allResultsCount, List<T> results)
         {
             AllResultsCount = allResultsCount;
             Results = results;
+        }
+
+        public void Remove(T entity)
+        {
+            Results.Remove(entity);
+            AllResultsCount--;
         }
     }
 }
